ecf18d794f1db76ab5bb3a4bfe39b9a840be0557
Ege Moroğlu <egemoroglu@hotmail.com>
2024-06-04 14:52:14 +0300

    TodoService

--- routes/handler.js
+++ routes/handler.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const todoService = require('../controller/TodoService')
+const todoService = require('../services/TodoService')
 
 router.get('/', async (req, res) => {
     try {
